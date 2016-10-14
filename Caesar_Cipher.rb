f4aecc4fd4fddbaf13dc0e9b570c021c00cf7f77
Sherif Shawky <sherif20766@hotmail.com>
2016-10-14 14:21:31 +0100

    Created main function for cipher, still a couple of issues to fix

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,19 +1,38 @@
-puts "Please enter a String"
+puts "Enter a string:"
 string = gets.chomp
 
-puts "please enter a Cipher Key"
-cipher_key = gets.chomp
+puts "Enter a cipher key:"
+num = gets.chomp.to_i
 
-def caesar_cipher(string, cipher_key)
-
-	split_string = string.split("")
-	
-	for i in 0..cipher_key
-		output = split_string[i].next
-		print output
-	end
-
+def back_shift(c)
+  (c.chr.ord - 26). chr
 end
 
+def func(string, num)
 
-caesar_cipher(string, cipher_key)
+	letters = string.split("")
+
+	letters.each do |x|
+
+		for i in 0...num
+			x = x.next!
+		end
+	end
+
+	letters.each do |x|
+
+		if x.length > 1
+			puts "this letter needs shifting back"
+					
+		end
+	end
+
+	output = letters.join
+
+	puts output.gsub(/[^0-9A-Za-z]/, " ")
+	
+	
+end
+
+func(string, num)
+
